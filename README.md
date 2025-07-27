6182fbae527dfa58c55b4db95ee94333d4b7a552
Covvalente <mateus.mrdo24@gmail.com>
2025-07-26 22:52:20 -0300

    enviando os arquivos principais do challenge

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# challenge-amigo-secreto_pt_Mateus
+# 🎁 Challenge: Amigo Secreto  
+**by Mateus Rodrigues**
+
+Este projeto consiste em um desafio de JavaScript, cujo objetivo é implementar um **sorteador de amigo secreto** de forma interativa.
+
+---
+
+## 🧠 Sobre o projeto
+
+O usuário pode adicionar os nomes dos participantes e, com um clique, o sistema realiza o sorteio, garantindo que **Todos os nomes sejam selecionados de maneira aleatória**.
+
+---
+
+## 📹 Demonstração
+
+Abaixo está a demonstração do funcionamento:
+
+![Demonstração do projeto](./Teste.gif)
+
+---
+
+## 🚀 Tecnologias utilizadas
+- HTML
+- CSS
+- JavaScript (Vanilla)
+
+---
+
+## 📁 Como executar
+
+1. Clone este repositório:
+   ```bash
+   git clone 
+
+## 📬 Contato
+
+Feito com ❤️ por **Mateus Rodrigues**  
+🔗 [LinkedIn](https://www.linkedin.com/in/mateus-rodrigues-913329168/)
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigos() {
+    let entrada = document.querySelector("#amigo");
+    let nome = entrada.value.trim();
+    if (nome == '') {
+        alert('Por Favor, Insira um nome válido.');
+        return;
+    }
+
+    amigos.push(nome);
+    entrada.value = '';
+    atualizaAmigos();
+    console.log(amigos);
+}
+
+function atualizaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigos() {
+    document.getElementById('listaAmigos').innerHTML = '';
+    if (amigos.length == 0) {
+        alert('Não há amigos para sortear');
+        return;
+    }
+
+    let nomesAleatorios = Math.floor(Math.random() * amigos.length);
+    let nomeSorteado = amigos[nomesAleatorios];
+
+    document.getElementById('resultado').innerHTML = `Amigo sorteado <strong>${nomeSorteado}</strong>`;
+}
+
